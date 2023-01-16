e4aa20606a7c059cf25341a399c29146b34f31a5
SankaraMoothi <sankaramoorthi99@gmail.com>
2023-01-16 13:17:46 +0530

    idUpdate

--- service/movie.service.js
+++ service/movie.service.js
@@ -1,10 +1,11 @@
+import { ObjectId } from "mongodb";
 import { client } from "../index.js";
 
 export async function editMovieById(id, data) {
   return await client
     .db("movie-data")
     .collection("movies")
-    .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function AddMovie(data) {
   return await client.db("movie-data").collection("movies").insertMany(data);
@@ -13,10 +14,13 @@ export async function deleteMovieById(id) {
   return await client
     .db("movie-data")
     .collection("movies")
-    .deleteOne({ id: id });
+    .deleteOne({ _id: ObjectId(id) });
 }
 export async function getMovieById(id) {
-  return await client.db("movie-data").collection("movies").findOne({ id: id });
+  return await client
+    .db("movie-data")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies() {
   return await client.db("movie-data").collection("movies").find({}).toArray();
