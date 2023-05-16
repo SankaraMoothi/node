a51738250430059676a88b9be3cabda557c09aba
SankaraMoothi <sankarmoorthi99@gmail.com>
2023-05-16 21:02:26 +0530

    cookieSessionUpdate

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import cookieSession from "cookie-session";
 const app = express();
 
 const PORT = process.env.PORT;
@@ -16,6 +16,17 @@ const mongo_url = process.env.MONGO_URL;
 const client = new MongoClient(mongo_url);
 client.connect();
 app.use(express.json());
+app.set("trust proxy", 1);
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberwolve"],
+    maxAge: 24 * 60 * 60 * 100,
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
+  })
+);
 app.use(
   cors({
     credentials: true,
